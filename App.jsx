a512fbbf01767aad64eb5ca654908be9708bb122
Danu06 <86379049+Danu06@users.noreply.github.com>
2021-12-21 23:36:04 -0300

    Navbar

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavbarCss from '../../my-app/NavbarCSS'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const App = () => {
+    return
+    (
+        <div className='hooks-app'>
+            <NavbarCss />
+        </div>
+    )
+}
+
+export default App
---
+++ NavbarCSS.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const NavbarCss = () => {
+    return
+        (
+            <div className='navbar'>
+                <h2>Cafe en grano</h2>
+                <nav>
+                <ul>
+                    <li>
+                        <a href='/#'>Inicio</a>
+                    </li>
+                    <li>
+                        <a href='/#'>Productos</a>
+                    </li>
+                    <li>
+                        <a href='/#'>Contacto</a>
+                    </li>
+                </ul>
+                </nav>
+            </div>
+
+    )
+}
+
+export default NavbarCss
